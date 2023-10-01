93612971262d2a258ed41052a1a406eaafc0d811
Martha <marthatwili@gmail.com>
2023-10-01 22:41:13 +0300

    Updated Notebook

---
+++ streamlit_project/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import xgboost as xgb
+
+#Creating the interface
+st.write("""# STORES SALES FORECASTING APP
+         This is a streamlit sales forecasting app
+""")
+
+#Create Expected Key Lists
